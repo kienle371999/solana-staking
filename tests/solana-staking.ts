b590ee0bb2b8baee20c3e603ae9f1062149ce184
kienle <kienle371999@gmail.com>
2023-12-29 23:52:26 +0700

    [SL-2] Add the demo smart contract and start FE codebase

--- tests/solana-staking.ts
+++ tests/solana-staking.ts
@@ -1,12 +1,12 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { Hello } from "../target/types/hello";
+import { SolanaStaking } from "../target/types/solana_staking";
 
-describe("hello", () => {
+describe("solana-staking", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.Hello as Program<Hello>;
+  const program = anchor.workspace.SolanaStaking as Program<SolanaStaking>;
 
   it("Is initialized!", async () => {
     // Add your test here.
